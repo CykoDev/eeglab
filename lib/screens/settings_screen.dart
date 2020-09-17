d0c9b28e83fa3a813dbb12a97e2c21483ebd5a20
Muhammad Saad <saad01.1999@gmail.com>
2020-09-17 22:21:49 +0500

    setup basic screens

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  static String routeName = '/login';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/signal_data_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignalDataScreen extends StatelessWidget {
+  static String routeName = '/login';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
