f3c6c94368f4538678c4f705d70fc8e059145691
Ailiya-Raza <59006842+Ailiya-Raza@users.noreply.github.com>
2020-09-27 16:27:24 +0500

    screens

---
+++ android/app/src/main/kotlin/com/example/eeglab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eeglab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,272 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:fl_animated_linechart/fl_animated_linechart.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title:new Text('SmartEEG',
+        style:TextStyle(
+          color: Colors.black,
+        )),
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children:[
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text('Welcome',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(35, 100, 20,  8),
+            child: Center(child: Text('Please enter your device in pairing mode and connect using the button below.',
+                style: TextStyle(
+                    fontSize: 25,
+                )
+            )),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: new RaisedButton(
+            child:Text('Connect to Device'),
+            color:Colors.green,
+            onPressed: (){
+              Navigator.push(
+                context,
+                new MaterialPageRoute(builder: (context) => new DeviceScreen()),
+              );
+            }),
+          )
+        ])
+      );
+  }
+}
+
+
+
+class DeviceScreen extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          leading: BackButton(
+              color: Colors.black
+          ),
+          title: new Text('SmartEEG',
+              style: TextStyle(
+                color: Colors.black,
+              )),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new SettingScreen()),
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.show_chart,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new SignalScreen()),
+                );
+              },
+            )
+          ],
+          backgroundColor: Colors.white,
+        ),
+        body: Center(
+          child: new Text (
+              'File Format',
+              style: TextStyle(
+                  fontSize: 30
+              )
+          ),
+        )
+    );
+  }
+
+}
+
+
+class SimpleLineChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  SimpleLineChart(this.seriesList, {this.animate});
+
+  /// Creates a [LineChart] with sample data and no transition.
+  factory SimpleLineChart.withSampleData() {
+    return new SimpleLineChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.LineChart(seriesList, animate: animate);
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<LinearSales, int>> _createSampleData() {
+    final data = [
+      new LinearSales(0, 5),
+      new LinearSales(1, 25),
+      new LinearSales(2, 100),
+      new LinearSales(3, 75),
+    ];
+
+    return [
+      new charts.Series<LinearSales, int>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample linear data type.
+class LinearSales {
+  final int year;
+  final int sales;
+
+  LinearSales(this.year, this.sales);
+}
+
+
+class SignalScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        leading: BackButton(
+            color: Colors.black
+        ),
+        title:new Text('SmartEEG',
+            style:TextStyle(
+              color: Colors.black,
+            )),
+        backgroundColor: Colors.white,
+      ),
+      body:
+        Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: SimpleLineChart.withSampleData(),
+        ),
+    );
+  }
+}
+
+
+
+
+
+
+class SettingScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: BackButton(
+            color: Colors.black
+        ),
+        title: new Text('SmartEEG',
+            style:TextStyle(
+              color: Colors.black,
+            )),
+        backgroundColor: Colors.white,
+      ),
+      body:
+      SettingsList(
+        sections: [
+          SettingsSection(
+            // title: 'Section',
+            tiles: [
+              SettingsTile.switchTile(
+                title: 'Impedance',
+                leading: Icon(Icons.fingerprint),
+                switchValue: true,
+                onToggle: (bool value) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Use fingerprint',
+                leading: Icon(Icons.fingerprint),
+                switchValue: true,
+                onToggle: (bool value) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Use fingerprint',
+                leading: Icon(Icons.fingerprint),
+                switchValue: true,
+                onToggle: (bool value) {},
+              ),
+              SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: Icon(Icons.language),
+                onTap: () {},
+              ),
+              SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: Icon(Icons.language),
+                onTap: () {},
+              ),
+
+            ]
+          ),
+        ],
+
+      ),
+
+      );
+  }
+}
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  void connectDevice(){
+    print('button pressed');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'SmartEEG',
+      home: new HomeScreen(),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
