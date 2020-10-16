54fe6b677345968ccf9096e7e63d48296eb42804
Anas Tasadduq <anas.tasadduq@gmail.com>
2020-10-16 15:08:15 +0500

    refactor code

--- lib/screens/signal_data_screen.dart
+++ lib/screens/signal_data_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/simple_line_chart.dart';
+
 class SignalDataScreen extends StatelessWidget {
-  static String routeName = '/login';
+  static String routeName = '/signal';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(),
+        title: new Text(
+          'SmartEEG',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SimpleLineChart.withSampleData(),
+      ),
+    );
   }
 }
